6262a18948ee30042da0f17a71424713ee788d88
Mateus Valerio <mateusvalerio1@gmail.com>
2023-02-04 10:47:36 -0300

    CONFIG: styled components added to the project
     The initial configs are done

--- src/index.jsx
+++ src/index.jsx
@@ -1,17 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import './common/global.css'
+import { ThemeProvider } from 'styled-components'
 import { AuthProvider } from './contexts/AuthContext'
 import MyRoutes from './routes/MyRoutes'
+import { GlobalStyles } from './styles/global.styled'
+import { theme } from './styles/theme'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
 	<React.StrictMode>
-		<BrowserRouter>
-			<AuthProvider>
-				<MyRoutes />
-			</AuthProvider>
-		</BrowserRouter>
+		<ThemeProvider theme={theme}>
+			<BrowserRouter>
+				<AuthProvider>
+					<MyRoutes />
+				</AuthProvider>
+			</BrowserRouter>
+			<GlobalStyles />
+		</ThemeProvider>
 	</React.StrictMode>,
 )
