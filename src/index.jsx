acabb64915e1f3ba6c5906229bf670a935cf45da
Mateus Valerio <mateusvalerio1@gmail.com>
2023-02-04 10:06:15 -0300

    CONFIG: remove materials ui from dependecies

--- src/index.jsx
+++ src/index.jsx
@@ -1,4 +1,3 @@
-import { createTheme, ThemeProvider } from '@mui/material'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
