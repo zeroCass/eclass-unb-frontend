a96b00ec37453d0c72379ef9973bf2e0cf036bab
pafev <paulofvilarimda@gmail.com>
2023-02-05 20:18:47 -0300

    componente ItemsContent

---
+++ src/components/ItemsContent/ItemsContent.jsx
@@ -0,0 +1,37 @@
+import { useMemo, useRef, useState } from 'react'
+import { Container } from './styles'
+import { BiSearchAlt } from 'react-icons/bi'
+import { SearchedItems } from '../SearchedItems'
+
+export const ItemsContent = ({ array, title, placeholder, itemFormat }) => {
+	const [text, setText] = useState('')
+	const searchedArray = useMemo(() => {
+		const lowerText = text.toLowerCase()
+		return array.filter((item) => item.toLowerCase().includes(lowerText))
+	}, [text, array])
+
+	const timeoutRef = useRef(null)
+	const delaySearch = 500 // (dado em ms)
+
+	return (
+		<Container>
+			<h1>{title}</h1>
+			<div className="searchBar">
+				<input
+					placeholder={placeholder}
+					onChange={(event) => {
+						window.clearTimeout(timeoutRef.current)
+						timeoutRef.current = window.setTimeout(() => {
+							setText(event.target.value)
+						}, delaySearch)
+					}}
+				></input>
+				<BiSearchAlt></BiSearchAlt>
+			</div>
+			<SearchedItems
+				searchedArray={searchedArray}
+				itemFormat={itemFormat}
+			></SearchedItems>
+		</Container>
+	)
+}
---
+++ src/components/ItemsContent/index.jsx
@@ -0,0 +1,1 @@
+export { ItemsContent } from './ItemsContent'
