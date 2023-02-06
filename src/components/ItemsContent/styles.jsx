b6407b3f77b61d1fa9c0baaca0d6777973653046
pafev <paulofvilarimda@gmail.com>
2023-02-05 21:27:01 -0300

    animação da barra de pesquisa

--- src/components/ItemsContent/styles.jsx
+++ src/components/ItemsContent/styles.jsx
@@ -8,6 +8,7 @@ export const Container = styled.section`
 	h1 {
 		margin: 2rem 0 1rem 0;
 		font-family: Neucha, sans-serif;
+		font-size: 2rem;
 	}
 	div {
 		width: 58%;
@@ -18,7 +19,7 @@ export const Container = styled.section`
 		> input {
 			width: 100%;
 			height: 100%;
-			padding: 0 2.5rem;
+			padding: 0 2.7rem;
 			border: 1px solid #d0cfce;
 			outline: none;
 			background: white;
@@ -28,11 +29,23 @@ export const Container = styled.section`
 				border: 1px solid rgba(0, 0, 0, 0.5);
 				transition: border 0.35s ease;
 				box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.3);
+				&::-webkit-input-placeholder {
+					transition: opacity 0.25s ease;
+					opacity: 0;
+				}
+				&::-moz-placeholder {
+					transition: opacity 0.25s ease;
+					opacity: 0;
+				}
+				&:-ms-placeholder {
+					transition: opacity 0.25s ease;
+					opacity: 0;
+				}
 			}
 		}
 		> .icon-search {
 			position: absolute;
-			right: 22%;
+			right: 23%;
 			top: 17.7rem;
 			font-size: 2rem;
 			/* margin-right: 2rem; */
