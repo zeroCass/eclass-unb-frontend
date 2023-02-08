3b28900e0879fdea5b8855b223ed2703a51dddb4
Maylla Krislainy <mayllak@hotmail.com>
2023-02-08 18:48:40 -0300

    REFACTOR: add "ItemsList" component in pages
    add the component in teachers and student pages

--- src/pages/Students/Students.jsx
+++ src/pages/Students/Students.jsx
@@ -1,11 +1,30 @@
+import { Button } from '../../components/Button'
+import { ItemList } from '../../components/ItemList/ItemList'
 import { ItemsContent } from '../../components/ItemsContent'
+
 export const Students = () => {
 	return (
 		<ItemsContent
 			title={'Estudantes'}
-			array={['Estudantes1', 'Estudantes2', 'Estudantes3']}
+			array={['Nome do Aluno 1', 'Nome do Aluno 2', 'Nome do Aluno 3']}
 			placeholder={'Pesquise por estudante'}
-			itemFormat={(item, index) => <li key={index}>{item} teste</li>}
+			itemFormat={(item, index) => (
+				<li key={index}>
+					<ItemList
+						title={item}
+						description="Matricula do Aluno"
+						buttonsComponents={[
+							<Button
+								key={0}
+								onClick={() => console.log('Visualizando Aluno')}
+								margin={'0'}
+							>
+								Visualizar
+							</Button>,
+						]}
+					/>
+				</li>
+			)}
 		/>
 	)
 }
--- src/pages/Teachers/Teachers.jsx
+++ src/pages/Teachers/Teachers.jsx
@@ -1,11 +1,30 @@
+import { Button } from '../../components/Button'
+import { ItemList } from '../../components/ItemList/ItemList'
 import { ItemsContent } from '../../components/ItemsContent'
+
 export const Teachers = () => {
 	return (
 		<ItemsContent
 			title={'Professores'}
 			array={['Professor1', 'Professor2', 'Professor3']}
 			placeholder={'Pesquise por professor'}
-			itemFormat={(item, index) => <li key={index}>{item} teste</li>}
+			itemFormat={(item, index) => (
+				<li key={index}>
+					<ItemList
+						title={item}
+						description="Matricula do Professor"
+						buttonsComponents={[
+							<Button
+								key={0}
+								onClick={() => console.log('Visualizando Professor')}
+								margin={'0'}
+							>
+								Visualizar
+							</Button>,
+						]}
+					/>
+				</li>
+			)}
 		/>
 	)
 }
