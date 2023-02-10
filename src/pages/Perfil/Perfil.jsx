33a63e8e4f34deabd49bbb45d00e5f65c48378cc
Maylla Krislainy <mayllak@hotmail.com>
2023-02-10 16:26:25 -0300

    REFACTOR: profile page buttons

--- src/pages/Perfil/Perfil.jsx
+++ src/pages/Perfil/Perfil.jsx
@@ -7,9 +7,20 @@ import { Image, Info, PageContainer } from './components/PerfilContainer'
 export const Perfil = () => {
 	const authContext = useContext(AuthContext)
 	const { authState } = authContext
-
-	// modal state
 	const [isOpen, setIsOpen] = useState(false)
+
+	const renderButton = (text) => (
+		<Button
+			margin={'30px'}
+			width={'50%'}
+			height={'55px'}
+			fontSize={'18px'}
+			key={0}
+			onClick={() => setIsOpen(true)}
+		>
+			{text}
+		</Button>
+	)
 
 	return (
 		<PageContainer>
@@ -22,50 +33,13 @@ export const Perfil = () => {
 						<p>Email: {authState.email}</p>
 						<p>Matricula: {authState.registrationID}</p>
 					</Info>
-					<Button
-						margin={'30px'}
-						width={'50%'}
-						height={'55px'}
-						fontSize={'18px'}
-						key={0}
-						onClick={() => setIsOpen(true)}
-					>
-						Alterar Senha
-					</Button>
+					{renderButton('Alterar Senha')}
 				</div>
 				<div className="buttons-div">
-					{authState.userType === 2 && (
-						<Button
-							margin={'30px'}
-							width={'50%'}
-							height={'55px'}
-							fontSize={'18px'}
-							key={0}
-							onClick={() => setIsOpen(true)}
-						>
-							Notas
-						</Button>
-					)}
-					<Button
-						margin={'30px'}
-						width={'50%'}
-						height={'55px'}
-						fontSize={'18px'}
-						key={0}
-						onClick={() => setIsOpen(true)}
-					>
-						Questões
-					</Button>
-					<Button
-						margin={'30px'}
-						width={'50%'}
-						height={'55px'}
-						fontSize={'18px'}
-						key={0}
-						onClick={() => setIsOpen(true)}
-					>
-						Avaliações
-					</Button>
+					{authState.userType != 0 && renderButton('Minhas Turmas')}
+					{authState.userType === 2 && renderButton('Notas')}
+					{renderButton('Questões')}
+					{renderButton('Avaliações')}
 				</div>
 			</div>
 		</PageContainer>
