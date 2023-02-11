09e765e6a33f3941e96f9b5ba36719a61278411a
Mateus Valerio <mateusvalerio1@gmail.com>
2023-02-11 15:40:05 -0300

    REFACTOR: SearchedItems

--- src/components/SearchedItems/SearchedItems.jsx
+++ src/components/SearchedItems/SearchedItems.jsx
@@ -1,9 +1,38 @@
+import { Button } from '../Button'
+import { ItemList } from '../ItemList'
 import { Container } from './styles'
 
-export const SearchedItems = ({ searchedArray, itemFormat }) => {
+const ButtonsComponent = ({ itemButtons }) => {
+	return (
+		<>
+			{itemButtons &&
+				itemButtons.map((button, index) => (
+					<Button margin={'0 10px'} key={index} onClick={button.onClick}>
+						{button.label}
+					</Button>
+				))}
+		</>
+	)
+}
+
+export const SearchedItems = ({ searchedArray, itemButtons }) => {
 	return (
 		<Container>
-			<ul>{searchedArray && searchedArray.map(itemFormat)}</ul>
+			<ul>
+				{searchedArray &&
+					searchedArray.map((item, index) => (
+						<ItemList
+							key={index}
+							title={item?.title}
+							description={item?.description}
+							hasIcon={item?.hasIcon}
+							timeDate={item?.timeDate}
+							buttonsComponents={
+								<ButtonsComponent itemButtons={itemButtons} />
+							}
+						/>
+					))}
+			</ul>
 		</Container>
 	)
 }
