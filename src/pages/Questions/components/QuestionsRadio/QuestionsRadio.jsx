513507cac7d660dbdf2d8b981b068ab43e2d340e
Mateus Valerio <mateusvalerio1@gmail.com>
2023-02-11 18:57:33 -0300

    STYLE: Question Radio buttons

--- src/pages/Questions/components/QuestionsRadio/QuestionsRadio.jsx
+++ src/pages/Questions/components/QuestionsRadio/QuestionsRadio.jsx
@@ -1,33 +1,31 @@
 /* eslint-disable */
+import * as Styled from './styles'
 
 export const QuestionsRadio = ({ questionType, setQuestionType }) => {
 	return (
-		<div>
-			<h4>Selecione </h4>
+		<Styled.Conainer>
 			<div>
-				<div>
-					<input
-						type={'radio'}
-						name="question-type"
-						value="discursive"
-						id="discursive"
-						checked={questionType === 'discursive'}
-						onChange={(e) => setQuestionType(e.target.value)}
-					/>
-					<label htmlFor="discursive">Dissertativa</label>
-				</div>
-				<div>
-					<input
-						type={'radio'}
-						name="question-type"
-						value="multipleChoice"
-						id="multipleChoice"
-						checked={questionType === 'multipleChoice'}
-						onChange={(e) => setQuestionType(e.target.value)}
-					/>
-					<label htmlFor="multipleChoice">Multipla Escolha</label>
-				</div>
+				<input
+					type={'radio'}
+					name="question-type"
+					value="discursive"
+					id="discursive"
+					checked={questionType === 'discursive'}
+					onChange={(e) => setQuestionType(e.target.value)}
+				/>
+				<label htmlFor="discursive">Dissertativa</label>
 			</div>
-		</div>
+			<div>
+				<input
+					type={'radio'}
+					name="question-type"
+					value="multipleChoice"
+					id="multipleChoice"
+					checked={questionType === 'multipleChoice'}
+					onChange={(e) => setQuestionType(e.target.value)}
+				/>
+				<label htmlFor="multipleChoice">Multipla Escolha</label>
+			</div>
+		</Styled.Conainer>
 	)
 }
---
+++ src/pages/Questions/components/QuestionsRadio/styles.js
@@ -0,0 +1,16 @@
+import styled from 'styled-components'
+
+export const Conainer = styled.div`
+	display: flex;
+	width: 400px;
+	justify-content: space-around;
+	padding: 10px;
+	font-weight: 400;
+	font-size: 18px;
+	line-height: 21px;
+	letter-spacing: 0.04em;
+	color: rgba(25, 51, 143, 0.8);
+	& label {
+		margin-left: 5px;
+	}
+`
