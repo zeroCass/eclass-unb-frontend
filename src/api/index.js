e9dd323ee6be422fd8ea9c15455016f324bbbac7
Mateus Valerio <mateusvalerio1@gmail.com>
2023-02-13 08:48:03 -0300

    CONFIG: api url updated

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-export const baseURL = 'http://127.0.0.1:8000'
+export const baseURL = 'http://127.0.0.1:8000/api/v1'
 
 export const fetchData = async (endpoint) => {
 	try {
